8773a615ef42d16281a87a2a1f930551791148f5
sdee <Dee@Sutees-MacBook-Air.local>
2014-09-01 21:38:01 -0700

    add basic form for tastings

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require "sinatra"
 require "sinatra/activerecord"
 
+class Tasting < ActiveRecord::Base
+end
+
 get '/' do
     "Hello, World!"
+end
+
+get '/new' do
+  @tasting = Tasting.new
+  erb :'new'
+end
+
+post '/tasting' do
+  Tasting.create(:sour => params[:sour])
+  erb :'show'
 end
