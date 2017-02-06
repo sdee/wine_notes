7c119fdd8f20ff77fc2751f59d113f306ebabd8a
Sutee Dee <thaiyoshi@gmail.com>
2017-02-05 21:38:47 -0500

    basic reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-
+import tasting from './tasting';
+import component from './component';
 
 const wine_notes = combineReducers({
-
+	component,
+	tasting
 });
 
 export default wine_notes;
